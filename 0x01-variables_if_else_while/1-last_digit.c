fa484bad9f6f4eb6c4354d8f52b790078422aa49
Maria <maria.bichinsky@gmail.com>
2018-06-22 06:57:50 +0000

    fixing errors that gcc with flags found for proj1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	else if (i == 0)
 		printf("Last digit of %d is %d and is 0\n", n, i);
-	else if (i < 6, i != 0)
+	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
 	return (0);
 }
