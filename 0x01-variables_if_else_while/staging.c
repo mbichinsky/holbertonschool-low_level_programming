b246ee99e85687e7860a1243e8123cba576c5e13
Maria <maria.bichinsky@gmail.com>
2018-06-22 06:33:56 +0000

    remade file 1 to try to pass checker

---
+++ 0x01-variables_if_else_while/staging.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- figure out what last digit of n is
+ * Description: prints last digit of n. says if greater than 5,
+ * is 0 or is less than 6 and not 0
+ * Return: Returns 0 if no issue
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	i = n % 10;
+	if (i > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	else if (i == 0)
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	else if (i < 6, i != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+	return (0);
+}
