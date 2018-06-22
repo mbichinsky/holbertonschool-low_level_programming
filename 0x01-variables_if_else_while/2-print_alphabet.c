6ab876bbaaa1d7249df58b140c35a76a25634fb3
Maria <maria.bichinsky@gmail.com>
2018-06-22 04:15:54 +0000

    working on p2. passed betty. prints a-z lowercase with new lines after each

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- print the alphabet
+ * Description: prints the alphabet in
+ * lowercase with a new line using putchar
+ * Return: Returns 0 if no issue
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
