9afab59cfd3d236d45a021101f1ba1189d94972c
Maria <maria.bichinsky@gmail.com>
2018-06-22 02:52:45 +0000

    Attempting prob 0. Added stdio header. Using if else and printf.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- figure out if n is positive or negative
+ * Description: prints if n is positive or negative              
+ * Return: Returns 0 if no issue
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n>0)
+		printf("%d is positive \n");
+	else if (n==0)
+		printf("%d is zero \n");
+	else if (n<0)
+		printf("%d is negative \n");
+	return (0);
+}
