6a3b070977da7289a5cf58b6b66251c36fa1377d
Maria <maria.bichinsky@gmail.com>
2018-06-26 20:57:48 +0000

    working on proj5. passed betty and gcc -Wall -pedantic -Werror -Wextra _putchar.c 5-main.c 5-sign.c -o 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_sign - says if number is pos/neg/zero
+ *
+ * @n: holds the character for checking
+ *
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	int p = 43;
+	int i = 45;
+	int z = 48;
+
+	if (n % 10 > 48)
+	{
+		_putchar(p);
+		return (1);
+	}
+	else if (n % 10 == 0)
+	{
+		_putchar(z);
+		return (0);
+	}
+	else if (n % 10 < 48)
+	{
+		_putchar(i);
+		return (-1);
+	}
+	return (2);
+}
+
