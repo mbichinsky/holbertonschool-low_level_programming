3f3e0a1e5f69fea43595f82e2a3e7a15c62176fb
Maria <maria.bichinsky@gmail.com>
2018-06-26 19:56:33 +0000

    working on 3-islower.c. passed betty and gcc -Wall -pedantic -Werror -Wextra _putchar.c 3-main.c 3-islower.c -o 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lower case character
+ *
+ * Return: 1 of c is lower, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
