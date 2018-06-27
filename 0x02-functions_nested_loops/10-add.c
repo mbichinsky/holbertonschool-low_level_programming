30abe6b7fbfa6d8baece8fd6952484bc9a1a16a1
Maria <maria.bichinsky@gmail.com>
2018-06-27 00:20:31 +0000

    working on 10, passed betty, gcc -Wall -pedantic -Werror -Wextra _putchar.c 10-main.c 10-add.c -o 10-add and give correct output.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - adds two ints together
+ *
+ * @i: first number
+ *
+ * @j: second number
+ *
+ * Return: result of i + j
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
