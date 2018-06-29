a96f85f123c4a6864c9a4e6cfe2c29fda5441e6b
Maria <maria.bichinsky@gmail.com>
2018-06-29 05:22:31 +0000

    working on 10. passes betty, gcc and output looks correct

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints diagonal lines
+ *
+ * @size: triangle size
+ *
+ * Return: Nothing.
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+			if (j > size - i)
+				_putchar ('#');
+			else
+				_putchar (' ');
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	_putchar ('\n');
+}
