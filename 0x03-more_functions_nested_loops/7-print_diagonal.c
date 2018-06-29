0582874a5e5b2b6c4f333b9a4fcd8e6c17b575ae
Maria <maria.bichinsky@gmail.com>
2018-06-29 00:38:00 +0000

    working on 7, passed betty, passed gcc, correct output

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints diagonal lines
+ *
+ * @n: number of diagonal lines
+ *
+ * Return: Nothing.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (i != j)
+				_putchar (32);
+				else
+				_putchar (92);
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	_putchar ('\n');
+}
