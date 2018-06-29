d38d1802e0942401cd28a86bef89cb38685a55dc
Maria <maria.bichinsky@gmail.com>
2018-06-29 04:50:35 +0000

    working on 8, passes betty, passes gcc, output looks correct

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_square - print a square using #
+ *
+ * @size: size of the square
+ *
+ * Return: Nothing.
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			for (i = 1; i <= size; i++)
+				_putchar ('*');
+				_putchar ('\n');
+		}
+	}
+	_putchar ('\n');
+}
