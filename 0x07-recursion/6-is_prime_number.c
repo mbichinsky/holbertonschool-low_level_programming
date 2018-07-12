4328a09abea0a63062c6066de7afe672d12ebe16
Maria <maria.bichinsky@gmail.com>
2018-07-12 01:20:53 +0000

    working on 6. passes gcc and betty. output correct

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * find_prime - finds the prime of n
+ *
+ * @n: number to be checked for
+ *
+ * @i: possible prime for n
+ *
+ * Return: 1 if integer is prime, else 0
+ */
+
+int find_prime(int i, int n)
+{
+	if (i * i == n)
+	{
+		return (0);
+	}
+	else if (i * i < n)
+	{
+		return (find_prime(i + 1, n));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - function looks for prime numbers
+ *
+ * @n: The number to find the prime of
+ *
+ * Return: find_prime
+ */
+
+int is_prime_number(int n)
+{
+	return (find_prime(0, n));
+}
