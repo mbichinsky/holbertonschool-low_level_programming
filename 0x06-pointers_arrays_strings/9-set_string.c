7d3f7921df1d9f5bb2d9e4f494d963b285944eed
Maria <maria.bichinsky@gmail.com>
2018-07-12 05:56:31 +0000

    working on 9. passes betty, gcc, correct output

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets value of pointer to char
+ *
+ * @s: pointer
+ *
+ * @to: char
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
