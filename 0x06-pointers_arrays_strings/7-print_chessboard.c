9866210a5e7028fdb886c51b8b468be844ff3a37
Maria <maria.bichinsky@gmail.com>
2018-07-13 05:21:36 +0000

    working on 7, passes betty, gcc and correct looking output

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints a chess board
+ *
+ * @a: array of chess pieces
+ *
+ * Return: Nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
