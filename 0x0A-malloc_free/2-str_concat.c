ea198f351dda95b54f100fa6d35d9fda8650453a
Maria <maria.bichinsky@gmail.com>
2018-07-18 01:45:35 +0000

    working on 2 and 3. they pass betty, have correct outputs and pass gcc

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * Return: NULL on fail or empty string, else pointer to memory
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, lens1, lens2;
+	char *pointer;
+
+	for (lens1 = 0; s1[lens1] != '\0'; lens1++)
+		;
+
+	for (lens2 = 0; s2[lens2] != '\0'; lens2++)
+		;
+
+	pointer = malloc(sizeof(char) * (lens1 + lens2));
+
+	for (i = 0; i < lens1 + lens2; i++)
+	{
+		pointer[i] = i < lens1 ? s1[i] : s2[i - lens1];
+	}
+	pointer[i] = '\0';
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+
+	return (pointer);
+}
---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - function that returns a pointer to a 2 dimensional
+ * array of integers
+ *
+ * @width: width of the string
+ *
+ * @height: height of the string
+ *
+ * Return: NULL on fail or if height/ width are zero. else 2 dimensional array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	array = malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	if (width <= 0 || height <= 0 || array == NULL)
+	{
+		return (NULL);
+	}
+
+	return (array);
+}
