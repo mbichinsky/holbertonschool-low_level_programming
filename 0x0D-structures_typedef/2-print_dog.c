c2591820fb0c7f13b6f3077bb3fc5c5ddae108b5
Maria <maria.bichinsky@gmail.com>
2018-07-24 05:05:24 +0000

    fixinginvalid float op.

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
-		printf("Age: %f\n", d->age == NULL ? "(nil)" : d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
 }
